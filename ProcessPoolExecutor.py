82c2bcdff4cf1bc96cb7480dd01ff06e9fcc8dd2
fuckseer <ksusha.magic.96@gmail.com>
2021-12-01 02:13:54 +0500

    ProcessPoolExecutor c 4 воркерами

--- ProcessPoolExecutor.py
+++ ProcessPoolExecutor.py
@@ -2,7 +2,7 @@ import concurrent.futures
 from hashlib import md5
 from random import choice
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
     while True:
         s = "".join([choice("0123456789") for i in range(50)])
         h = md5(s.encode('utf8')).hexdigest()
